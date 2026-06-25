ef352d8969872f25b2aef7695c54b971c8dfe768
Abbas Ali <oggamer48italia@gmail.com>
2026-06-25 15:16:31 +0500

    Initial commit

---
+++ Chapter 1/index.js
@@ -0,0 +1,20 @@
+// 1. Write a script to greet your website visitor using JS alert box
+alert("Welcome To Our Website");
+// 2. Write a script to display following message on your web page:
+alert("Error! Please enter a valid password.");
+// 3. Write a script to display following message on your web page:(Hint : Use line break) 
+alert("Welcome to JS Land... \n Happy Coding!");
+// 4. Write a script to display following messages in sequence:
+alert("Welcome to JS Land...");
+alert(" Happy Coding!");
+// 5. Generate the following message through browser’s developer console:
+// Answer: Messege was generated on browser
+// 6. Make use of alerts in your new/existing HTML & CSS project.
+// Answer: already used
+// 7. Practice placement of <script></script> element in following sections of your project in exercise 6: 
+// a. Head 
+// b. Body (before your page’s HTML)
+// c. Body (inside your page’s HTML) 
+// d. Body (after your page’s HTML)
+// Answer: Done in index.html file
+alert("Chapter 1 Assignment Completed ");
