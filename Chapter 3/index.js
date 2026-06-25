a60b193c5b8e3134e65b80ad20b50e929ec1d557
Abbas Ali <oggamer48italia@gmail.com>
2026-06-25 16:51:29 +0500

    Assignment Chapter 3

---
+++ Chapter 3/index.js
@@ -0,0 +1,20 @@
+// 1. Declare a variable called age & assign to it your age. Show your age in an alert box. 
+var age = 16;
+alert(age);
+// 2. Declare & initialize a variable to keep track of how many times a visitor has visited a web page. Show his/her number of visits on your web page. For example: “You have visited this site N times”.
+var visits = 14;
+alert("You have visited this site " + visits + " times");
+// 3. Declare a variable called birthYear & assign to it your birth year. Show the following message in your browser:
+var birthYear = 2011;
+document.write("My Birth Year Is " + birthYear + "<br>Data Type Of My Declared Variable Is " + typeof birthYear);
+// 4. A visitor visits an online clothing store www.xyzClothing.com . Write a script to store in variables the following information:
+// a. Visitor’s name
+// b. Product title 
+// c. Quantity i.e. how many products a visitor wants to 
+// order 
+// Show the following message in your browser: “John 
+// Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+var visName = "Abbas Ali";
+var visProduct = "T-Shirts";
+var visProductQuantity = 15;
+document.write("<br>" + visName + " Ordered " + visProductQuantity + " " + visProduct);
